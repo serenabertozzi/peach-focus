529429919d27a99f69bb8de9f8326b4190e2f067
serenabertozzi <serenabertozzi@protonmail.com>
2021-12-20 15:19:49 +0100

    Add timer logic

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -3,16 +3,62 @@ import "react-circular-progressbar/dist/styles.css";
 import PauseButton from "./PauseButton";
 import PlayButton from "./PlayButton";
 import SettingsButton from "./SettingsButton";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import SettingsContext from "./SettingsContext";
 
-const percentage = 60;
 const red = "#F5605B";
 const green = "#D5D066";
 
 const Timer = () => {
   const settingsInfo = useContext(SettingsContext);
   const [isPaused, setIsPaused] = useState(true);
+  const [mode, setMode] = useState("work"); // work/break/null
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  const secondsLeftRef = useRef(secondsLeft);
+  const isPausedRef = useRef(isPaused);
+  const modeRef = useRef(mode);
+
+  function switchMode() {
+    const nextMode = modeRef.current === "work" ? "break" : "work";
+    const nextSeconds =
+      (nextMode === "work"
+        ? settingsInfo.workMinutes
+        : settingsInfo.breakMinutes) * 60;
+    setMode(nextMode);
+    modeRef.current = nextMode;
+
+    setSecondsLeft(nextSeconds);
+    secondsLeftRef.current = nextSeconds;
+  }
+
+  function tick() {
+    secondsLeftRef.current--;
+    setSecondsLeft(secondsLeftRef.current);
+  }
+
+  function initTimer() {
+    setSecondsLeft(settingsInfo.workMinutes * 60);
+  }
+
+  useEffect(() => {
+    initTimer();
+
+    const interval = setInterval(() => {
+      if (isPausedRef.current) {
+        return;
+      }
+      if (secondsLeftRef.current === 0) {
+        return switchMode();
+      }
+
+      tick();
+    }, 1000);
+
+    return clearInterval(interval);
+  }, [settingsInfo]);
+
+  const percentage = 60;
 
   return (
     <div>
