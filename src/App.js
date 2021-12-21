2278db8496a39bf8ee6f5bb3c47e69d76b4ebe84
serenabertozzi <serenabertozzi@protonmail.com>
2021-12-21 12:42:05 +0100

    Refactor Logo into component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Settings from "./Components/Settings";
 import Timer from "./Components/Timer";
 import { useState } from "react";
 import SettingsContext from "./Components/SettingsContext";
+import Logo from "./Components/Logo";
 
 function App() {
   const [showSettings, setShowSettings] = useState(false);
@@ -11,22 +12,7 @@ function App() {
 
   return (
     <main>
-      <div className="logo">
-        <svg
-          style={{ width: "10%", marginRight: "10px" }}
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-            clip-rule="evenodd"
-          />
-        </svg>
-        <h1>Peach Timer</h1>
-      </div>
+      <Logo />
       <SettingsContext.Provider
         value={{
           showSettings,
