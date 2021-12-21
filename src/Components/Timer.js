b522d62b727c117d941049bd588bec893285fa71
serenabertozzi <serenabertozzi@protonmail.com>
2021-12-21 11:41:29 +0100

    Add color changing between work and rest mode

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -5,12 +5,14 @@ import PlayButton from "./PlayButton";
 import SettingsButton from "./SettingsButton";
 import { useContext, useState, useEffect, useRef } from "react";
 import SettingsContext from "./SettingsContext";
+import Helmet from "react-helmet";
 
 const red = "#F5605B";
 const green = "#D5D066";
 
-const Timer = () => {
+function Timer() {
   const settingsInfo = useContext(SettingsContext);
+
   const [isPaused, setIsPaused] = useState(true);
   const [mode, setMode] = useState("work"); // work/break/null
   const [secondsLeft, setSecondsLeft] = useState(0);
@@ -19,30 +21,28 @@ const Timer = () => {
   const isPausedRef = useRef(isPaused);
   const modeRef = useRef(mode);
 
-  function switchMode() {
-    const nextMode = modeRef.current === "work" ? "break" : "work";
-    const nextSeconds =
-      (nextMode === "work"
-        ? settingsInfo.workMinutes
-        : settingsInfo.breakMinutes) * 60;
-    setMode(nextMode);
-    modeRef.current = nextMode;
-
-    setSecondsLeft(nextSeconds);
-    secondsLeftRef.current = nextSeconds;
-  }
-
   function tick() {
     secondsLeftRef.current--;
     setSecondsLeft(secondsLeftRef.current);
   }
 
-  function initTimer() {
-    setSecondsLeft(settingsInfo.workMinutes * 60);
-  }
-
   useEffect(() => {
-    initTimer();
+    function switchMode() {
+      const nextMode = modeRef.current === "work" ? "break" : "work";
+      const nextSeconds =
+        (nextMode === "work"
+          ? settingsInfo.workMinutes
+          : settingsInfo.breakMinutes) * 60;
+
+      setMode(nextMode);
+      modeRef.current = nextMode;
+
+      setSecondsLeft(nextSeconds);
+      secondsLeftRef.current = nextSeconds;
+    }
+
+    secondsLeftRef.current = settingsInfo.workMinutes * 60;
+    setSecondsLeft(secondsLeftRef.current);
 
     const interval = setInterval(() => {
       if (isPausedRef.current) {
@@ -55,40 +55,59 @@ const Timer = () => {
       tick();
     }, 1000);
 
-    return clearInterval(interval);
+    return () => clearInterval(interval);
   }, [settingsInfo]);
 
   const totalSeconds =
     mode === "work"
       ? settingsInfo.workMinutes * 60
-      : settingsInfo.breakMinutes * 600;
-  const percentage = Math.round(secondsLeft / totalSeconds) * 100;
+      : settingsInfo.breakMinutes * 60;
+  const percentage = Math.round((secondsLeft / totalSeconds) * 100);
 
   const minutes = Math.floor(secondsLeft / 60);
   let seconds = secondsLeft % 60;
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
+  if (seconds < 10) seconds = "0" + seconds;
 
   return (
     <div>
+      <Helmet
+        bodyAttributes={
+          mode === "work"
+            ? { style: "background-color : #f4b9a0;" }
+            : { style: "background-color : #3ba193;" }
+        }
+      />
       <CircularProgressbar
         value={percentage}
-        text={`${minutes}:${seconds}`}
+        text={minutes + ":" + seconds}
         styles={buildStyles({
           textColor: "#fff",
-          pathColor: red,
-          trailColor: "rgba(255,255,255,0.2",
+          pathColor: mode === "work" ? red : green,
+          tailColor: "rgba(255,255,255,.2)",
         })}
       />
-      <div style={{ marginTop: "2rem" }}>
-        {isPaused ? <PlayButton /> : <PauseButton />}
+      <div style={{ marginTop: "20px" }}>
+        {isPaused ? (
+          <PlayButton
+            onClick={() => {
+              setIsPaused(false);
+              isPausedRef.current = false;
+            }}
+          />
+        ) : (
+          <PauseButton
+            onClick={() => {
+              setIsPaused(true);
+              isPausedRef.current = true;
+            }}
+          />
+        )}
       </div>
-      <div style={{ marginTop: "2rem" }}>
+      <div style={{ marginTop: "20px" }}>
         <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
       </div>
     </div>
   );
-};
+}
 
 export default Timer;
