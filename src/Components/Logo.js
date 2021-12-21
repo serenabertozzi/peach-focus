7ad3cd23170e8c4d311c40820fff998d57f6a33c
serenabertozzi <serenabertozzi@protonmail.com>
2021-12-21 12:51:01 +0100

    Add icon, title and changed logo

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,20 +1,7 @@
 const Logo = () => {
   return (
-    <div className="logo">
-      <svg
-        style={{ width: "10%", marginRight: "10px" }}
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <h1>Peach Timer</h1>
+    <div style={{ width: "100%" }} className="logo">
+      <h1>🍑 Peach Focus</h1>
     </div>
   );
 };
